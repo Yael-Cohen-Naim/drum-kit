2b3edc8d01ef7fe6eaea51c3224b7741c29f0d23
Yael Cohen Naim <52420120+Yael-Cohen-Naim@users.noreply.github.com>
2020-10-28 13:51:46 +0200

    upload files

---
+++ index.js
@@ -0,0 +1,75 @@
+//Detecting keyboard press
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+});
+
+//Detecting button press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.innerHTML;
+
+    makeSound(buttonInnerHtml);
+
+    buttonAnimation(buttonInnerHtml);
+  });
+}
+
+//Drum sound function
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+
+      break;
+
+    default:
+      console.log(key);
+      a;
+  }
+}
+
+// Animation function
+function buttonAnimation(currentKey) {
+  //Selected button
+  var activeButton = document.querySelector("." + currentKey);
+
+  //Start animation
+  activeButton.classList.add("pressed");
+
+  //End ani,ation
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
